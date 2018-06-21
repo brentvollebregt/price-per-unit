06897ea82b1785995d1404c3b8e9759cf13105e5
Brent Vollebregt <volleys2468@gmail.com>
2018-06-21 23:10:04 +1200

    Header and control bar created

--- app/src/main/java/net/nitratine/priceperunit/MainActivity.java
+++ app/src/main/java/net/nitratine/priceperunit/MainActivity.java
@@ -2,6 +2,8 @@ package net.nitratine.priceperunit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner UnitTypeSpnr = (Spinner) findViewById(R.id.UnitTypeSpnr);
+        String[] items = new String[]{"Weight", "Volume", "Length", "Pieces"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.spinner_item, items);
+        UnitTypeSpnr.setAdapter(adapter);
     }
 }
