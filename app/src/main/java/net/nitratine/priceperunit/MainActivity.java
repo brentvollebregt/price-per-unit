bf63be9e78a7fcd85cccec97e62127f015b74084
Brent Vollebregt <brent.vollebregt@gmail.com>
2021-02-22 21:17:13 +1300

    Update target SDK version to 30

--- app/src/main/java/net/nitratine/priceperunit/MainActivity.java
+++ app/src/main/java/net/nitratine/priceperunit/MainActivity.java
@@ -1,7 +1,7 @@
 package net.nitratine.priceperunit;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
